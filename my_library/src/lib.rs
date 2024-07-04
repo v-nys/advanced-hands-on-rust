dfb89caa0e6a789d4af34aa9f02aa5bad486a89e
Vincent Nys <vincentnys@gmail.com>
2024-07-04 11:31:42 +0200

    RNG tests.

---
+++ my_library/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod rand {
+    pub use rand::*;
+}
+
+mod random;
+
+pub use random::*;
--- shell.nix
+++ shell.nix
@@ -7,9 +7,14 @@ mkShell rec {
     pkg-config
   ];
   buildInputs = [
-    udev alsa-lib vulkan-loader
+    udev alsa-lib vulkan-loader vulkan-tools
     xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
     libxkbcommon wayland # To use the wayland feature
   ];
   LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+  shellHook = ''
+    export WGPU_BACKEND=vulkan
+    export VK_INSTANCE_LAYERS=VK_LAYER_KHRONOS_validation
+  '';
 }
